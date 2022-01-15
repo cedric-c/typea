544fa2e72fc7ecda7137f7f4b0adfb9f4e99b345
Cédric Clément <8315076+cedric-c@users.noreply.github.com>
2022-01-15 10:44:25 -0500

    fixed reqs, autoformat code

--- typea.py
+++ typea.py
@@ -6,7 +6,7 @@ from typea.candidates import Candidates
 from copy import copy
 
 articles = Candidates()
-print("Attempting to rename",len(articles.pdfs), "files")
+print("Attempting to rename", len(articles.pdfs), "files")
 
 skipped = 0
 renamed = 0
@@ -15,34 +15,33 @@ renamed = 0
 for file in articles.pdfs:
     authors, title, date = articles.name(file)
     original_name = ntpath.basename(file)
-    
-    if(date != None):
+
+    if (date != None):
         date = date[2:6]
-    
+
     date = date if date != None else ''
-    
-    
-    if(authors == title == None or title == None or authors == title == '' or title == ''):
+
+    if (authors == title == None or title == None or authors == title == '' or title == ''):
         print("Skipping file (missing attributes):", file)
         skipped += 1
         articles.clone(filepath=file, new_name=original_name, add_ext=False)
         continue
-    
-    if(authors == None or authors == '' ):
+
+    if (authors == None or authors == ''):
         print("Warning: No author(s) in meta")
         renamed += 1
         articles.clone(filepath=file, new_name=title)
         continue
-    
-    if(len(authors.split(" ")) == 1):
+
+    if (len(authors.split(" ")) == 1):
         print("Broken authors")
         skipped += 1
         articles.clone(filepath=file, new_name=original_name, add_ext=False)
         continue
-        
+
     firstAuthor = authors.split(" ")[1]
-    
-    articles.clone(filepath=file, new_name=date + " - " +firstAuthor + " - " + title)
+
+    articles.clone(filepath=file, new_name=date + " - " + firstAuthor + " - " + title)
     renamed += 1
-    
-print("Renamed",renamed,"files, skipped",skipped)
+
+print("Renamed", renamed, "files, skipped", skipped)
--- typea/candidates.py
+++ typea/candidates.py
@@ -1,9 +1,11 @@
 #!/usr/local/bin/python3.7
 # -*- coding: utf-8 -*-
 
-import os, shutil
+import os
+import shutil
 from os import listdir
 from os.path import isfile, join
+
 import PyPDF2
 
 TARGET_EXTENSION = ".pdf"
@@ -11,30 +13,32 @@ OUTPUT_DIRECTORY = "renamed_articles"
 
 ARTICLE_CREATOR = '/Creator'
 ARTICLE_PRODUCER = '/Producer'
-ARTICLE_AUTHOR   = '/Author'
-ARTICLE_TITLE   = '/Title'
+ARTICLE_AUTHOR = '/Author'
+ARTICLE_TITLE = '/Title'
 ARTICLE_SUBJECT = '/Subject'
-ARTICLE_DATE    = '/CreationDate'
-IEEE_ARTICLE_ID   = '/IEEE#20Issue#20ID' # IEEE
+ARTICLE_DATE = '/CreationDate'
+IEEE_ARTICLE_ID = '/IEEE#20Issue#20ID'  # IEEE
+
+
 # ARTICLE_PUBLICATION_ID = '/IEEE#20Publication#20ID' # IEEE
 
 class Candidates:
     """This is a potential candidate for renaming"""
-    
-    def __init__(self, directory = os.getcwd()):
+
+    def __init__(self, directory=os.getcwd()):
         """(Candidates, str) -> NoneType"""
         self.directory = directory
         self.pdfs = self.list(self.directory)
         self.infos = self.meta(self.directory)
-    
-    def list(self, directory = None):
+
+    def list(self, directory=None):
         """(Candidates, str) -> dictionary of str
         Lists the files in the provided directory which have a pdf extension.        
         """
-        
-        if(directory == None):
+
+        if (directory == None):
             directory = self.directory
-        
+
         filenames = []
         path = os.path.expanduser(directory)
         for file in listdir(path):
@@ -47,24 +51,24 @@ class Candidates:
                     filenames.append(name)
 
         return filenames
-    
-    def meta(self, directory = None):
+
+    def meta(self, directory=None):
         """(Candidates) -> dictionary of str
         Returns the files and their metadata as a dictionary indexed on the filenames of the files.
         """
-        
-        if(directory == None):
+
+        if (directory == None):
             directory = self.directory
-        
+
         meta = {}
         for filename in self.pdfs:
-            if(self.readable(filename)):
+            if (self.readable(filename)):
                 pdf_object = open(filename, 'rb')
                 reader = PyPDF2.PdfFileReader(pdf_object)
                 meta[filename] = reader.getDocumentInfo()
-            
+
         return meta
-    
+
     def readable(self, filename):
         """(Candidates) -> str
         Returns True if PyPDF can read the PDF and/or parse the attributes.
@@ -75,34 +79,34 @@ class Candidates:
             return True
         except:
             return False
-    
-    def clone(self, filepath, new_name = None, destination_dir = None, add_ext = True):
+
+    def clone(self, filepath, new_name=None, destination_dir=None, add_ext=True):
         """(Candidates) -> name of target file str, name of target destination_dir dir, str.
         Copies the file at the fully qualified filepath to destination_dir.
         If no destination_dir is provided, copies to OUTPUT_DIRECTORY.
         
         Creates the folder starting from the path first specified when creating the object.
         """
-        
-        if(destination_dir == None):
+
+        if (destination_dir == None):
             destination_dir = OUTPUT_DIRECTORY
-        
-        if(not os.path.exists(destination_dir)):
+
+        if (not os.path.exists(destination_dir)):
             os.makedirs(destination_dir)
 
         new_dir = join(self.directory, destination_dir)
         new_path = new_name + TARGET_EXTENSION if add_ext else new_name
         shutil.copy(filepath, join(new_dir, new_path))
-    
+
     def name(self, filepath):
         """(Candidates) -> name of target file str
         Returns the name of a file by searching through the metadata.
         """
         author = self.__getAuthor(filepath)
-        title  = self.__getTitle(filepath)
-        date   = self.__get(ARTICLE_DATE, filepath)
+        title = self.__getTitle(filepath)
+        date = self.__get(ARTICLE_DATE, filepath)
         return author, title, date
-    
+
     def __getAuthor(self, filepath):
         """This is a WIP.
         """
@@ -111,8 +115,7 @@ class Candidates:
             return data[ARTICLE_AUTHOR]
         except:
             return None
-        
-        
+
     def __getTitle(self, filepath):
         """This is a WIP
         """
@@ -121,9 +124,8 @@ class Candidates:
             # print(data,"hello")
             return data[ARTICLE_TITLE]
         except:
-            return None        
-    
-    
+            return None
+
     def __get(self, key, filepath):
         """This is a WIP.
         """
@@ -132,6 +134,7 @@ class Candidates:
             return data[key]
         except:
             return None
+
 
 if __name__ == "__main__":
     c = Candidates()
